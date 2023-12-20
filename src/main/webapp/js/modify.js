ab7678ed120e6a808d939e0ed36a3c52e7057094
LingLing <ryoung6920@gmail.com>
2023-12-20 16:21:58 +0900

    231220 16:21 - 레이어 삭제에서 피쳐 삭제로 변경 (regist / modify)

--- src/main/webapp/js/modify.js
+++ src/main/webapp/js/modify.js
@@ -1,37 +1,34 @@
 $(document).ready(function(){
 	
-	// 1. 해당 맛집 지도 및 마커 띄우기
+	// 1. 해당 맛집 지도레이어 + 마커레이어 + 피쳐 가져오기
 	init();
-	addMarker(centerPos)
+	markerSource = getLS(centerPos);
 
-	// 2. 지도 클릭 시 마커 생성
+	// 2. 지도 클릭 시 피쳐 생성
     map.on('click', function(evt) {
         
-    	// 1) marker 레이어 있는지 먼저 탐색, 있다면 삭제 (실행 순서 활용)
-    	// -> 하나의 마커만 띄우기 위함
-    	// -> 필터하면 배열로 리턴됨 보통 이름 같은 레이어는 하나밖에 없으니까 0번지로 지정
-    	map.removeLayer(map.getLayers().getArray().filter(i => i.get('id') == 'marker')[0]); 
-
+    	// 1) markerSource에 피쳐 있는지 먼저 탐색, 있다면 삭제 
+    	// (실행 순서 활용해서 마커 피쳐 하나만 띄우기)
+    	// 방법1
+    	if(markerSource.getFeatures().length != 0) {
+    		markerSource.removeFeature(markerSource.getFeatures().filter(i => i.get('id') == 'originMarkerFeature' || i.get('id') == 'newMarkerFeature')[0]);
+    	}
+    	// 방법2
+//    	markerSource.getFeatures().clear();
+    	
     	// 2) 클릭한 지점 경위도 좌표값 가져오기
     	let lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-    	console.log(lonlat);
-    	
-        let xxx = Number(lonlat[0]);
-        let yyy = Number(lonlat[1]);
-        console.log("x = " + xxx + " y = " + yyy);
-        
-        let x = document.getElementById('xxx');
-        let y = document.getElementById('yyy');
-        x.value = xxx;
-        y.value = yyy;
-        
-        // 마커 띄울 위치
-        // 좌표체계가 이미 변환된 값
-    	centerPos = [xxx, yyy];
-        addMarker(centerPos);
+    	console.log("x = " + Number(lonlat[0]) + " y = " + Number(lonlat[1]));
+        // View 페이지에 좌표값 전달
+        document.getElementById('xxx').value = Number(lonlat[0]);
+        document.getElementById('yyy').value = Number(lonlat[1]);
+
+        // 3) 새로운 피쳐 생성
+    	centerPos = [Number(lonlat[0]), Number(lonlat[1])];
+    	addFT(markerSource, centerPos);
     });
 	
-	// 3. '수정하기' 클릭
+	// 3. '수정하기' 클릭 시
 	let frObj = $('#fr'); // form태그 정보 저장
 	$('#update').click(function(){
 		alert("수정이 완료되었습니다.");
@@ -40,59 +37,81 @@ $(document).ready(function(){
 		frObj.submit();
 	});
 });
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJQueryㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJQueryㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 
 
-//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJSㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-//ㅡㅡㅡㅡㅡㅡㅡㅡ 1. 특정 맛집 지도 생성 ㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJSㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 1. 특정 맛집 지도 레이어 생성 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 function init() {
 			    map = new ol.Map({
-			 		   layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
-					   target: 'map',
-					   view: new ol.View({
-						  center: ol.proj.transform(centerPos, 'EPSG:4326', 'EPSG:3857'),
-						  zoom: 17
-						})
+						 		   layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
+								   target: 'map',
+								   view: new ol.View({
+													  center: ol.proj.transform(centerPos, 'EPSG:4326', 'EPSG:3857'),
+													  zoom: 17
+									})
 			    });
 }
 
-//ㅡㅡㅡㅡㅡㅡㅡㅡ 2. 마커 레이어 ㅡㅡㅡㅡㅡㅡㅡㅡ
-//마커 레이어 추가 메서드 정의
-function addMarker(centerPos) {
-	
-	let markerLayer;
-	
-	// 1) 마커 레이어에 들어갈 소스 생성
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 2. 기존 마커 레이어 + 소스 가져오기 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+function getLS(centerPos) {
+		
+	// 1) 마커 소스 생성
 	let markerSource = new ol.source.Vector();
 	
-	 // 2) Feture 좌표 등록
-	 let point_feature = new ol.Feature({
-	     geometry: new ol.geom.Point(centerPos).transform('EPSG:4326', 'EPSG:3857') 
-	 });
+	// 2) 기존 피쳐 추가
+	let originFeature = new ol.Feature({
+										id: "originMarkerFeature",
+	    								geometry: new ol.geom.Point(centerPos).transform('EPSG:4326', 'EPSG:3857') 
+						});
+	markerSource.addFeature(originFeature);
 
-	// 3) 소스에 feture 추가 
-//      addFeature를 이용해서, 여러개의 point를 source에 담음
-	 markerSource.addFeature(point_feature);
+	// 3) 마커 스타일 지정	
+//	let markerStyle = new ol.style.Style({
+//										   image: new ol.style.Icon({ // 마커 이미지
+//																      opacity: 1, 			   // 투명도 1 = 100% 
+//																      scale: 0.1, 			   // 크기 1 = 100%
+//																      src: '/img/marker.png'   // 해당 이미지로 변경
+//										   }),
+//										   zindex: 10 // html의 css, z-index 기능
+//	});
 
-// 4) 마커 스타일 추가
-//    style을 활용해서, point의 style을 변경
-	 let markerStyle = new ol.style.Style({
-	    image: new ol.style.Icon({  // 마커 이미지
-	        opacity: 1, 			// 투명도 1 = 100% 
-	        scale: 0.1, 			// 크기 1 = 100%
-	        src: '/img/marker.png'  // 해당 point를 marker로 변경해줌
-	    }),
-	    zindex: 10					// html의 css, z-index 기능
-	});
-
-// 5) 마커 레이어 생성
-	markerLayer = new ol.layer.Vector({
-		id: "marker",
-	    source: markerSource, // 마커 feacture들
-	    style: markerStyle    // 마커 스타일
-	});
+	// 4) 마커 레이어 조합
+	let markerLayer = new ol.layer.Vector({
+											id: "markerLayer",
+										    source: markerSource
+//										    style: markerStyle
+					  });
 	
 	// 6) 지도에 마커가 그려진 레이어 추가
 	map.addLayer(markerLayer);
+	
+	return markerSource;
+}
+
+
+
+// 3. ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ새로운 피쳐 추가하기ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+function addFT(markerSource, centerPos) {
+	
+	// 1) 피쳐 생성
+	let newFeature = new ol.Feature({
+									  id: "newMarkerFeature",
+									  geometry: new ol.geom.Point(centerPos).transform('EPSG:4326', 'EPSG:3857')
+					 });
+	
+	// 2) 스타일 지정
+	let markerStyle = new ol.style.Style({
+										   image: new ol.style.Icon({ // 마커 이미지
+																      opacity: 1, 			   // 투명도 1 = 100% 
+																      scale: 0.1, 			   // 크기 1 = 100%
+																      src: '/img/marker.png'   // 해당 이미지로 변경
+										   }),
+										   zindex: 10 // html의 css, z-index 기능
+					  });
+	newFeature.setStyle(markerStyle);
+	
+	// 3) 소스에 추가
+	markerSource.addFeature(newFeature);
 }
--- src/main/webapp/js/regist.js
+++ src/main/webapp/js/regist.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
 		
-	// 지도 생성
+	// 1. 지도 생성
 	init();
 	
-	// 마커 '레이어 + 소스' 생성
-//	let aMarkerSource = addML();
-	addML();
-	// return markerSource
+	// 2. 마커 '레이어 + 소스' 생성
+	addML(); // return markerSource
 	
-	// 지도 클릭 시 소스에 피쳐 추가
+	// 3. 지도 클릭 시 소스에 피쳐 추가
     map.on('click', function(evt) {
     	
     	// 1) marker 피쳐가 있는지 먼저 탐색, 있다면 삭제
     	// -> 하나의 마커만 띄우기 위함
+    	// * 레이어 삭제
 //    	map.removeLayer(map.getLayers().getArray().filter(i => i.get('id') == 'marker')[0]);
+    	console.log(markerSource.getFeatures().filter(i => i.get('id') == 'markerFeature')[0]);	// undefined
+    	
+    	// 방법1 - if문
+    	if(markerSource.getFeatures().length != 0) {
+    		markerSource.removeFeature(markerSource.getFeatures().filter(i => i.get('id') == 'markerFeature')[0]);
+    	}
+    	// 방법2 - 모두 삭제
+//    	markerSource.clear();
     	
     	// 2) 클릭한 지점 경위도 좌표값 가져오기
     	let lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-    	
-        let xxx = Number(lonlat[0]);
-        let yyy = Number(lonlat[1]);
-        console.log("x = " + xxx + " y = " + yyy);
-        
-        document.getElementById('xxx').value = xxx;
-        document.getElementById('yyy').value = yyy;
+        console.log("x = " + Number(lonlat[0]) + " y = " + Number(lonlat[1]));
+        // View 페이지에 좌표 값 전달
+        document.getElementById('xxx').value = Number(lonlat[0]);
+        document.getElementById('yyy').value = Number(lonlat[1]);
 
         // 3) 마커 피쳐 추가
         // -> 좌표체계가 이미 변환된 값이니 바로 대입
-    	centerPos = [xxx, yyy];
+    	centerPos = [Number(lonlat[0]), Number(lonlat[1])];
         addFT(markerSource, centerPos);
         
     });
     
 });
-// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJQueryㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJQueryㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 
 
-// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJSㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-// ㅡㅡㅡㅡㅡㅡㅡㅡ 1. 회사 중심 지도 생성 ㅡㅡㅡㅡㅡㅡㅡㅡ
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡJSㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 1. 회사 중심 지도 생성 ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 function init() {
     map = new ol.Map({
@@ -52,18 +56,30 @@ function init() {
 }
 
 
-//ㅡㅡㅡㅡㅡㅡㅡㅡ 2. 마커 레이어 생성ㅡㅡㅡㅡㅡㅡㅡㅡ
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 2. 마커 레이어 생성ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 
 // 마커 레이어 + 소스 추가
 function addML() {
 	
 	// 1) 마커 레이어에 들어갈 소스 생성
 	markerSource = new ol.source.Vector();
+	
+	// 2) 스타일 지정
+	let markerStyle = new ol.style.Style({
+										   image: new ol.style.Icon({ // 마커 이미지
+																      opacity: 1, 			   // 투명도 1 = 100% 
+																      scale: 0.1, 			   // 크기 1 = 100%
+																      src: '/img/marker.png'   // 해당 이미지로 변경
+										 }),
+										 zindex: 10 // html의 css, z-index 기능
+					  });
 
-	// 2) 마커 레이어 생성
+	// 3) 마커 레이어 생성
 	let markerLayer = new ol.layer.Vector({
 											id: "markerLayer",
-										    source: markerSource
+										    source: markerSource,
+										    style: markerStyle
 					  });
 	
 	// 3) 지도에 마커 레이어 추가
@@ -73,26 +89,17 @@ function addML() {
 }
 
 
-//ㅡㅡㅡㅡㅡㅡㅡㅡ 3. 마커 피쳐 생성ㅡㅡㅡㅡㅡㅡㅡㅡ
-function addFT(markerSource, centerPos){
+
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ 3. 마커 피쳐 추가ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+function addFT(markerSource, centerPos) {
 	
-	// 1) style 설정
-	let markerStyle = new ol.style.Style({
-										   image: new ol.style.Icon({ // 마커 이미지
-																      opacity: 1, 			   // 투명도 1 = 100% 
-																      scale: 0.1, 			   // 크기 1 = 100%
-																      src: '/img/marker.png'   // 해당 이미지로 변경
-										   }),
-										   zindex: 10				   // html의 css, z-index 기능
-					  });
-	
-	// 2) 피쳐 생성
+	// 1) 피쳐 생성
 	let ft = new ol.Feature({
 							 id: "markerFeature",
-	    					 geometry: new ol.geom.Point(centerPos).transform('EPSG:4326', 'EPSG:3857'),
-	    					 style: markerStyle
+	    					 geometry: new ol.geom.Point(centerPos).transform('EPSG:4326', 'EPSG:3857')
+//	    					 style: markerStyle
 			 });
  
-	// 3) 소스에 피쳐 추가
+	// 2) 소스에 피쳐 추가
 	markerSource.addFeature(ft);
 }
